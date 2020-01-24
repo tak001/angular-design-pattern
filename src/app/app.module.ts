4d3afb2adf9b76c16e9605846c945ca86a100ee5
taikou.kobayashi <twitwitwi2@yahoo.co.jp>
2020-01-24 12:40:09 +0900

    feat:add factory-method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { TemplateMethodComponent } from './template-method/template-method.component';
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FactoryMethodComponent } from "./factory-method/factory-method.component";
+import { HeaderComponent } from "./layout/header/header.component";
+import { TemplateMethodComponent } from "./template-method/template-method.component";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      HeaderComponent,
-      TemplateMethodComponent
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TemplateMethodComponent,
+    FactoryMethodComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
