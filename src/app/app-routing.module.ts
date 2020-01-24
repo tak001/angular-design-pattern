232e0d38e91ee3704ea8c826a01d96877eda60ae
taikou.kobayashi <twitwitwi2@yahoo.co.jp>
2020-01-24 12:43:38 +0900

    chore:formmated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { TemplateMethodComponent } from './template-method/template-method.component';
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { FactoryMethodComponent } from "./factory-method/factory-method.component";
-import { HeaderComponent } from "./layout/header/header.component";
-import { TemplateMethodComponent } from "./template-method/template-method.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FactoryMethodComponent } from './factory-method/factory-method.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { TemplateMethodComponent } from './template-method/template-method.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    TemplateMethodComponent,
-    FactoryMethodComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, TemplateMethodComponent, FactoryMethodComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
